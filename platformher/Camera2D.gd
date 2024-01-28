c05cb5445ac9a214caef69349baf1bd1cbf91d09
JaveCraze <javemanga@gmail.com>
2024-01-27 22:26:32 -0500

    crouch cam thing + level simplifiede

--- platformher/Camera2D.gd
+++ platformher/Camera2D.gd
@@ -1,4 +1,5 @@
 extends Camera2D
+#@onready var player = Gamemanager.player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,8 +8,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if Gamemanager.player.lookinleft == true:
+	if Gamemanager.player.lookinleft:
 		position.x = lerp(position.x, 30.0, .2)
 	else:
 		position.x = lerp(position.x, 30.0, .2)
 	pass
+	if Gamemanager.player.state == Gamemanager.player.Crouching:
+		offset.y = lerp(offset.y, 100.0, .2)
+	else:
+		offset.y = lerp(offset.y, 0.0, .2)
+	if Gamemanager.player.state == Gamemanager.player.Jumping or Gamemanager.player.state == Gamemanager.player.Falling:
+		position_smoothing_speed = 7
+	else:
+		position_smoothing_speed = 10
+
